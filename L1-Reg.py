ebbca7ed9132d6fa0151740ebe1d793dbb3eaa32
nishgaba-ai <nishgaba.ai@gmail.com>
2017-09-08 12:29:14 +0100

    Delete L1-Reg.py

--- L1-Reg.py
+++
@@ -1,5 +0,0 @@
-# L1 reg
-with tf.name_scope('train'):
-    train_step = tf.train.FtrlOptimizer(learning_rate=0.001,l1_regularization_strength=0.3).minimize(error)
-    
-init = tf.global_variables_initializer()
