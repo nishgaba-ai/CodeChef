4c2c2e0045c7f9745164f37046699204413fc1d7
nishgaba-ai <nishgaba.ai@gmail.com>
2017-09-08 12:28:49 +0100

    Delete Ftrl-Optimizer.py

--- Ftrl-Optimizer.py
+++
@@ -1,6 +0,0 @@
-            
-# FtrlOptimizer
-with tf.name_scope('train'):
-    train_step = tf.train.FtrlOptimizer(learning_rate=0.01).minimize(error)
-    
-init = tf.global_variables_initializer()
