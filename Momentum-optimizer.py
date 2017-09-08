212b1e92461e40b45afd42a335d5072c804de902
nishgaba-ai <nishgaba.ai@gmail.com>
2017-09-08 12:29:42 +0100

    Delete Momentum-optimizer.py

--- Momentum-optimizer.py
+++
@@ -1,6 +0,0 @@
-    
-# MomentumOptimizer
-with tf.name_scope('train'):
-    train_step = tf.train.MomentumOptimizer(learning_rate=0.01, momentum = 0.005).minimize(error)
-    
-init = tf.global_variables_initializer()
