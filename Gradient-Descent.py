07dc1e9eca0a16790855649769a287fb178e6fde
nishgaba-ai <nishgaba.ai@gmail.com>
2017-09-08 12:28:59 +0100

    Delete Gradient-Descent.py

--- Gradient-Descent.py
+++
@@ -1,5 +0,0 @@
-#GradientDescentOptimizer
-with tf.name_scope('train'):
-    train_step = tf.train.GradientDescentOptimizer(learning_rate=0.01, name='GradientDescent').minimize(error)
-    
-init = tf.global_variables_initializer()
